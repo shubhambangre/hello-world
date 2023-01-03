a45fa91ff929469fa2e83acaa268a33c6810fda6
shubhambangre <105714706+shubhambangre@users.noreply.github.com>
2023-01-03 19:31:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('hello-world')
+    res.end('hello-world rnt')
   })
 const PORT = process.env.port || 8080
   server.listen (PORT, () => console.log('Listening'))
